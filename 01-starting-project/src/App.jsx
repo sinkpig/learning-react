accf2d7c96666926de35b6eb77f0e4dd6e84cacb
Milo Ramirez <sinkpig00@gmail.com>
2025-01-10 17:03:05 -0300

    added dynamic image

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,3 +1,5 @@
+import iconImage from './assets/react-core-concepts.png'
+
 const listDescriptions = ['Fundamental', 'Crucial', 'Core']
 
 function genRandomInt(max) {
@@ -8,7 +10,7 @@ function Header() {
   const description = listDescriptions[genRandomInt(listDescriptions.length - 1)]
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={iconImage} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
