eac10c18222c4c14458784a4575b3004030f36c6
Milo Ramirez <sinkpig00@gmail.com>
2025-01-15 18:51:17 -0300

    dynamically changed text when clicked

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,10 +1,28 @@
+import { useState } from 'react'
+
 import Header from './components/Header/Header.jsx'
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx'
 import TabButton from './components/TabButton/TabButton.jsx'
+import { EXAMPLES } from './data.js'
 
 function App() {
-  function selectHandler() {
-    console.log('clicked')
+  const [ selectedTopic, setSelectedTopic ] = useState()
+  let tabContent = <p>Select a topic to learn more.</p>
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    )
+  }
+
+  function selectHandler(topic) {
+    setSelectedTopic(topic)
   }
 
   return (
@@ -20,11 +38,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={selectHandler}>Components</TabButton>
-            <TabButton onSelect={selectHandler}>JSX</TabButton>
-            <TabButton onSelect={selectHandler}>Props</TabButton>
-            <TabButton onSelect={selectHandler}>State</TabButton>
+            <TabButton onSelect={() => selectHandler('components')}>Components</TabButton>
+            <TabButton onSelect={() => selectHandler('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => selectHandler('props')}>Props</TabButton>
+            <TabButton onSelect={() => selectHandler('state')}>State</TabButton>
           </menu>
+          {tabContent}
         </section>
       </main>
     </div>
