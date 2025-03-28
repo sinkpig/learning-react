57f9da4e3c4341047f59e9f35b2a1385ffc027b8
sinkpig <sinkpig00@gmail.com>
2025-03-28 11:29:57 -0300

    improved app component for readability

--- 02-tic-tac-toe-starting-project/src/App.jsx
+++ 02-tic-tac-toe-starting-project/src/App.jsx
@@ -6,30 +6,42 @@ import GameBoard from "./components/GameBoard/GameBoard"
 import Log from "./components/Log/Log"
 import GameOver from "./components/GameOver/GameOver"
 
-const firstSymbol = '🌝'
-const secondSymbol = '🌚'
-const initialGameBoard = [
+const FIRST_SYMBOL = '🌝'
+const SECOND_SYMBOL = '🌚'
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null]
 ]
+const PLAYERS = {
+  [FIRST_SYMBOL]: 'Player 1',
+  [SECOND_SYMBOL]: 'Player 2'
+}
 
 function deriveActivePlayer(gameTurns) {
-  let currentSymbol = firstSymbol
-  if (gameTurns.length > 0 && gameTurns[0].symbol === firstSymbol) currentSymbol = secondSymbol
+  let currentSymbol = FIRST_SYMBOL
+  if (gameTurns.length > 0 && gameTurns[0].symbol === FIRST_SYMBOL) currentSymbol = SECOND_SYMBOL
   return currentSymbol
 }
 
-function App() {
-  const [players, setPlayers] = useState({
-    [firstSymbol]: 'Player 1',
-    [secondSymbol]: 'Player 2'
-  })
-  const [gameTurns, setGameTurns] = useState([])
-  const activePlayer = deriveActivePlayer(gameTurns)
-  let gameBoard = [...initialGameBoard.map(array => [...array])]
+function deriveWinner(gameBoard, players) {
   let winner
-  const isDraw = gameTurns.length === 9 && !winner
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstTileSymbol = gameBoard[combination[0].row][combination[0].column]
+    const secondTileSymbol = gameBoard[combination[1].row][combination[1].column]
+    const thirdTileSymbol = gameBoard[combination[2].row][combination[2].column]
+
+    if (firstTileSymbol && firstTileSymbol === secondTileSymbol && firstTileSymbol === thirdTileSymbol) {
+      winner = players[firstTileSymbol]
+    }
+  }
+
+  return winner
+}
+
+function deriveGameBoard(gameTurns) {
+  let gameBoard = [...INITIAL_GAME_BOARD.map(array => [...array])]
 
   for (const turn of gameTurns) {
     const  { tile, symbol } = turn
@@ -37,6 +49,17 @@ function App() {
 
     gameBoard[row][column] = symbol
   }
+
+  return gameBoard
+}
+
+function App() {
+  const [players, setPlayers] = useState(PLAYERS)
+  const [gameTurns, setGameTurns] = useState([])
+  const activePlayer = deriveActivePlayer(gameTurns)
+  const gameBoard = deriveGameBoard(gameTurns)
+  const winner = deriveWinner(gameBoard, players)
+  const isDraw = gameTurns.length === 9 && !winner
 
   function handleChangePlayerName(symbol, updatedName) {
     setPlayers(prevPlayers => {
@@ -69,15 +92,15 @@ function App() {
       <div id="game-container">
         <ol id="players" className="highlight-player">
           <PlayerInfo
-            initialName="Player 1"
-            symbol={firstSymbol}
-            isActive={activePlayer === firstSymbol}
+            initialName={PLAYERS[FIRST_SYMBOL]}
+            symbol={FIRST_SYMBOL}
+            isActive={activePlayer === FIRST_SYMBOL}
             onChangePlayerName={handleChangePlayerName}
           />
           <PlayerInfo
-            initialName="Player 2"
-            symbol={secondSymbol}
-            isActive={activePlayer === secondSymbol}
+            initialName={PLAYERS[SECOND_SYMBOL]}
+            symbol={SECOND_SYMBOL}
+            isActive={activePlayer === SECOND_SYMBOL}
             onChangePlayerName={handleChangePlayerName}
           />
         </ol>
